85af2c246c4b1dde1a6b43428fc28f9793c9312d
Jacob D. Parr <me@jacobparr.com>
2022-04-08 15:08:28 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-# Databricks notebook source
 import setuptools
+from setuptools import find_packages
 
 setuptools.setup(
     name="dbacademy-courseware",
     version="0.1",
     package_dir={"dbacademy": "src"},
-    packages=["dbacademy.dbpublish", "dbacademy.dbtest"],
+    packages=find_packages(),
+    # packages=["dbacademy", "dbacademy.dbtest", "dbacademy.dbrest"],
 )
