4e585603ae25f5f219fe301ce361ceb697ab7077
Jacob D. Parr <me@jacobparr.com>
2022-04-08 14:25:20 -0500

    Fixing package resolution

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setuptools.setup(
     name="dbacademy-courseware",
     version="0.1",
     packages=['dbacademy'],
-    package_dir={"dbacademy": "./Source"},
+    package_dir={"dbacademy": "Source"},
 )
