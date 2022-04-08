d6cee022341d889634207b728db9ee2b0d08377f
Jacob D. Parr <me@jacobparr.com>
2022-04-08 15:09:44 -0500

    x

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setuptools.setup(
     name="dbacademy-courseware",
     version="0.1",
     package_dir={"dbacademy": "src"},
-    packages=find_packages(),
-    # packages=["dbacademy", "dbacademy.dbtest", "dbacademy.dbrest"],
+    # packages=find_packages(),
+    packages=["dbacademy", "dbacademy.dbtest", "dbacademy.dbrest"],
 )
