aa76c15c092e5a0b67ec960b691fda84257c575b
Jacob D. Parr <me@jacobparr.com>
2022-04-08 17:55:37 -0500

    Update notebook_def_class.py

--- src/dbpublish/notebook_def_class.py
+++ src/dbpublish/notebook_def_class.py
@@ -189,7 +189,7 @@ class NotebookDef:
         self.validate_md_link(i, command, other_notebooks)
         self.validate_html_link(i, command)
 
-        lines = command.strip("\n")
+        lines = command.split("\n")
         multiple_lines = self.warn(lambda: len(lines) > 1, f"Expected MD in command #{i+1} to have more than 1 line of code")
 
         if self.i18n and multiple_lines:
