5cfb58cdec4b7a9bb6b5fd29c683d6bb2bf6b805
Jacob D. Parr <me@jacobparr.com>
2022-04-08 15:03:52 -0500

    Refactoring layout

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ import setuptools
 setuptools.setup(
     name="dbacademy-courseware",
     version="0.1",
-    package_dir={"dbacademy": "Source"},
-    packages=['dbacademy'],
+    package_dir={"dbacademy": "src"},
+    packages=["dbacademy.dbpublish", "dbacademy.dbtest"],
 )
