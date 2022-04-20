5c994460e3d7c7a938a8ebd328125757d2c61bfb
Jacob D. Parr <me@jacobparr.com>
2022-04-20 10:17:18 -0500

    Updated create_test_job() to set the spark_env variable required to enable writes to the local file system

--- src/dbtest/__init__.py
+++ src/dbtest/__init__.py
@@ -202,6 +202,9 @@ def create_test_job(client, test_config, job_name, notebook_path):
             "instance_pool_id": f"{test_config.instance_pool}",
             "spark_version": f"{test_config.spark_version}",
             "spark_conf": test_config.spark_conf
+        },
+        "spark_env_vars": {
+            "WSFS_ENABLE_WRITE_SUPPORT": "true"
         }
     }
     json_response = client.jobs().create(params)
