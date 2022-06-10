6b9d638c4e7b3803c82df37d1d3d06d4bf46aea2
Jacob D. Parr <jacob.parr@databricks.com>
2022-06-10 18:06:10 -0500

    Update resource_diff.py

--- src/dbpublish/resource_diff.py
+++ src/dbpublish/resource_diff.py
@@ -55,12 +55,12 @@ class ResourceDiff:
                     rows = max(len(change.original_text.split("\n")), len(change.latest_text.split("\n")))+1
 
                     html += f"""<tr><td colspan="2" style="padding:0">
-                        <table style="width:1600px"><tr>
+                        <table style="width:1600px; border-collapse: collapse; border-spacing:0"><tr>
                             <td style="width:800px; vertical-align:top; overflow-x:scroll;">
-                                <textarea rows="{rows}" style="width:800px; white-space:pre;">{change.original_text}</textarea>
+                                <textarea rows="{rows}" style="padding:0; width:800px; white-space:pre;">{change.original_text}</textarea>
                             </td>
                             <td style="width:800px; vertical-align:top; overflow-x:scroll;">
-                                <textarea rows="{rows}" style="width:800px; white-space:pre;">{change.latest_text}</textarea>
+                                <textarea rows="{rows}" style="padding:0; width:800px; white-space:pre;">{change.latest_text}</textarea>
                             </td>
                         </tr></table>
                     </td></tr>"""
