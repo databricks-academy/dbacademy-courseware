ec3a405779b054ddc97bb7659c6d20067747dd45
Jacob D. Parr <jacob.parr@databricks.com>
2022-06-10 16:46:42 -0500

    Update resource_diff.py

--- src/dbpublish/resource_diff.py
+++ src/dbpublish/resource_diff.py
@@ -50,8 +50,6 @@ class ResourceDiff:
             for change in sd.diff():
                 html += f"""<tr><td style="white-space:nowrap">{change.change_type}</td>
                                 <td>{change.message}</td>
-                                <td><a href="/#{self.repo_dir}/{self.original_resource}/{sd.name}">original</a></td>
-                                <td><a href="/#{self.repo_dir}/{self.latest_resource}/{sd.name}">latest</a></td>
                             </tr>"""
             html += f"""</tbody>"""
 
