81ce8fe002b78d2d7abf5a98a069a804d5197ac1
Jacob D. Parr <jacob.parr@databricks.com>
2022-08-08 17:58:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,23 +6,10 @@ reqs = [
     "dbacademy-rest@git+https://github.com/databricks-academy/dbacademy-rest",
 ]
 
-
-def find_dbacademy_packages():
-    packages = find_packages(where="src")
-
-    if "dbacademy" in packages:
-        del packages[packages.index("dbacademy")]
-
-    print("-"*80)
-    print(packages)
-    print("-"*80)
-    return packages
-
-
 setuptools.setup(
     name="dbacademy-courseware",
     version="0.1",
     install_requires=reqs,
     package_dir={"": "src"},
-    packages=find_dbacademy_packages()
+    packages=find_packages(where="src")
 )
