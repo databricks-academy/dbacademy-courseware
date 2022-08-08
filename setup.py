fd481c2a4b8c4d6645a2480c2064959ed3339ed9
Jacob D. Parr <jacob.parr@databricks.com>
2022-08-08 18:18:44 -0500

    Refactoring

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 import setuptools
 from setuptools import find_packages
 
-reqs = [
-    "requests",
-    "dbacademy-rest@git+https://github.com/databricks-academy/dbacademy-rest",
-]
+def find_dbacademy_packages():
+    packages = find_packages(where="src")
+    if "dbacademy" in packages:
+        del packages[packages.index("dbacademy")]
+    # print("-"*80)
+    # print(packages)
+    # print("-"*80)
+    return packages
+
 
 setuptools.setup(
     name="dbacademy-courseware",
     version="0.1",
-    install_requires=reqs,
     package_dir={"": "src"},
-    packages=find_packages(where="src")
+    packages=find_dbacademy_packages(),
+    install_requires=[
+        "requests",
+        "dbacademy-rest@git+https://github.com/databricks-academy/dbacademy-rest",
+    ],
 )
---
+++ src/dbacademy/dbtest/__init__.py
@@ -0,0 +1,9 @@
+import dbacademy_courseware.dbtest as dba_test
+
+to_job_url = dba_test.to_job_url
+to_job_link = dba_test.to_job_link
+create_test_job = dba_test.create_test_job
+
+TestConfig = dba_test.TestConfig
+TestInstance = dba_test.TestInstance
+TestSuite = dba_test.TestSuite
