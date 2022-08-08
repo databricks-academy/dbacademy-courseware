9adf0e83d986f80cc799e14be54be8a1c014ca2c
Jacob D. Parr <jacob.parr@databricks.com>
2022-08-08 18:26:33 -0500

    Update __init__.py

--- src/dbacademy/dbtest/__init__.py
+++ src/dbacademy/dbtest/__init__.py
@@ -1,5 +1,10 @@
 import dbacademy_courseware.dbtest as dba_test
 
+print("*" * 80)
+print("* DEPRECATION WARNING")
+print("* dbacademy.dbtest has been replaced by dbacademy_courseware.dbtest")
+print("*" * 80)
+
 to_job_url = dba_test.to_job_url
 to_job_link = dba_test.to_job_link
 create_test_job = dba_test.create_test_job
