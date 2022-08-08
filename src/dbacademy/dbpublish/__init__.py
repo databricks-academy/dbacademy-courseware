fd4b9be793b747c2392995e045729ecce85a563a
Jacob D. Parr <jacob.parr@databricks.com>
2022-08-08 18:26:01 -0500

    Update __init__.py

--- src/dbacademy/dbpublish/__init__.py
+++ src/dbacademy/dbpublish/__init__.py
@@ -1,5 +1,10 @@
 import dbacademy_courseware.dbpublish as dba_pub
 
+print("*" * 80)
+print("* DEPRECATION WARNING")
+print("* dbacademy.dbpublish has been replaced by dbacademy_courseware.dbpublish")
+print("*" * 80)
+
 help_html = dba_pub.help_html
 from_test_config = dba_pub.from_test_config
 update_and_validate_git_branch = dba_pub.update_and_validate_git_branch
