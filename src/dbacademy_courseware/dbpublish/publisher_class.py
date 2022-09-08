697725df052f64d7a711bff581e10d20a9a6f496
SireInsectus <jacob.parr@databricks.com>
2022-09-08 13:30:11 -0500

    Update publisher_class.py

--- src/dbacademy_courseware/dbpublish/publisher_class.py
+++ src/dbacademy_courseware/dbpublish/publisher_class.py
@@ -179,7 +179,7 @@ Change Log:\n"""
             message += "\n"
 
         message += f"""
-\nRelease notes, course-specific requirements, issue-tracking, and test results for this course can be found in the course's GitHub repository at https://github.com/databricks-academy/{source_repo.split("/")[-1]}
+\n\nRelease notes, course-specific requirements, issue-tracking, and test results for this course can be found in the course's GitHub repository at https://github.com/databricks-academy/{source_repo.split("/")[-1]}
 
 Please feel free to reach out to me (via Slack), or anyone on the curriculum team should you have any questions.""".strip()
 
