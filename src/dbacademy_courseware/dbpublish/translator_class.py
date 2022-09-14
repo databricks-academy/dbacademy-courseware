56476c5c44b6734d6f27bfcb648d05be41986fb5
SireInsectus <jacob.parr@databricks.com>
2022-09-14 18:49:38 -0500

    Update translator_class.py

--- src/dbacademy_courseware/dbpublish/translator_class.py
+++ src/dbacademy_courseware/dbpublish/translator_class.py
@@ -100,6 +100,7 @@ class Translator:
         import os
 
         if path.startswith("Solutions/"): path = path[10:]
+        if path.startswith("Includes/"): return ""
 
         i18n_source_path = f"/Workspace{self.resources_folder}/{self.i18n_language}/{path}.md"
         assert os.path.exists(i18n_source_path), f"Cannot find {i18n_source_path}"
