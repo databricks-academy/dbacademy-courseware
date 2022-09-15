0dd37440022c211469de445e5262c2008e9409a0
SireInsectus <jacob.parr@databricks.com>
2022-09-14 21:11:25 -0500

    Update translator_class.py

--- src/dbacademy_courseware/dbpublish/translator_class.py
+++ src/dbacademy_courseware/dbpublish/translator_class.py
@@ -66,7 +66,7 @@ class Translator:
     def _reset_source_repo(self, source_dir: str = None, source_repo_url: str = None, source_branch: str = None):
         from dbacademy_courseware.dbpublish import Publisher
 
-        self.source_branch = source_branch or f"published-{self.core_version}"
+        self.source_branch = source_branch or f"published-v{self.core_version}"
         self.source_dir = source_dir or f"/Repos/Working/{self.build_name}-english_{self.source_branch}"
         self.source_repo_url = source_repo_url or f"https://github.com/databricks-academy/{self.build_name}-english.git"
 
