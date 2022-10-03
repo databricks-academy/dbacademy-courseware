e4695dad6ea0d73a040427ceea4e99cbdfaf9b49
SireInsectus <jacob.parr@databricks.com>
2022-10-02 20:29:58 -0500

    Bug fixes

--- pip-install.bat
+++ pip-install.bat
@@ -2,5 +2,5 @@ cls
 call pip uninstall -y dbacademy-gems
 call pip uninstall -y dbacademy-rest
 pause
-call pip install git+https://github.com/databricks-academy/dbacademy-gems@dev --no-cache-dir
+call pip install git+https://github.com/databricks-academy/dbacademy-gems --no-cache-dir
 call pip install git+https://github.com/databricks-academy/dbacademy-rest --no-cache-dir
